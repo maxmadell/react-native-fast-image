8002c2889e286b8751cb333eb1f5e41f28071deb
Adam McArthur <adam@adammcarthur.net>
2019-02-01 03:30:57 +1100

    Add suport for resizeImageAndroid prop

--- src/index.d.ts
+++ src/index.d.ts
@@ -44,6 +44,11 @@ declare namespace FastImage {
         | FastImage.cacheControl.web
 }
 
+export type ResizeImageAndroid = {
+    width: number
+    height: number
+}
+
 export type FastImageSource = {
     uri?: string
     headers?: { [key: string]: string }
@@ -85,6 +90,7 @@ export interface FastImageProperties {
     source: FastImageSource | number
     placeholder?: React.ReactNode
     resizeMode?: FastImage.ResizeMode
+    resizeImageAndroid?: ResizeImageAndroid
     fallback?: boolean
 
     onLoadStart?(): void
